27d3c101fc56d4fd79acc0207f9fa39745baf9c7
Majd Taby <mtaby@me.com>
2011-08-15 14:47:12 -0700

    css_hooks are a separate package now

--- packages/sproutcore-touch/lib/system.js
+++ packages/sproutcore-touch/lib/system.js
@@ -1,4 +1,3 @@
-require('sproutcore-touch/system/css_hooks');
 require('sproutcore-touch/system/gesture');
 require('sproutcore-touch/system/view');
 require('sproutcore-touch/system/gesture_manager');
